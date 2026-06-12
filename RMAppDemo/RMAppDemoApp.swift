9b01ca3e4cb4852f48459768d3169bb35e3cc9db
Osciel Perea <bio_osc@hotmail.com>
2026-06-12 13:50:14 -0600

    Initial Commit

---
+++ RMAppDemo/RMAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMAppDemoApp.swift
+//  RMAppDemo
+//
+//  Created by BioOsc on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RMAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
